a4dfda76fd4cc4daa80d82d00f657892d42610bd
POWERFULZHENG <98935569+POWERFULZHENG@users.noreply.github.com>
2023-08-30 17:14:11 +0800

    在进度条显示和一些状态信息显示的基础上，增加了第三个界面的内容，能够在tableWidget中增加一行信息，局限：仅仅能加一行信息

--- test3.py
+++ test3.py
@@ -1,4 +1,5 @@
 import threading
+import typing
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5 import QtGui
@@ -13,8 +14,11 @@ import os
 import time
 
 class UploadThread(QThread):
+    """第一个页面设计"""
     progressUpdated = pyqtSignal(int)  # 上传进度信号
     finished = pyqtSignal(int)  # 上传完成信号
+    """第三个页面设计"""
+    resultReceived = pyqtSignal(str)
     
     def __init__(self, file_paths):
         super(UploadThread, self).__init__()
@@ -26,7 +30,8 @@ class UploadThread(QThread):
         print("当前活动线程数：", self.active_threads) 
         
     def run(self):
-        print("-----UploadThread线程开启------")     
+        print("-----UploadThread线程开启------")
+        """第一个页面设计"""   
         total_files = len(self.file_paths)
         uploaded_files = 0
         total_progress = 0     
@@ -43,7 +48,11 @@ class UploadThread(QThread):
             # 发送总体进度信号
                 self.progressUpdated.emit(total_progress)
         if 100 == total_progress:    
-            self.finished.emit(1)                 
+            """第三个页面设计"""
+            result = "上传完成"
+            self.resultReceived.emit(result)
+            #关闭上传完成线程                 
+            self.finished.emit(1)
     def cancel_upload(self):
         self.is_canceled = True
 
@@ -52,25 +61,29 @@ class MW(Ui_Form, QWidget):
     def __init__(self, parent=None):
         super(MW, self).__init__()
         self.setupUi(self)
-        
+        """第一个页面设计"""         
+        #与图片读取相关变量
         self.imgName = []  # 用来存储全部图片的名字
         self.imgPathName = []  # 用来存储全部图片的路径+名字
         self.imgPathNames = []  # 用来存储图片的路径+名字
+        #按键触发
         self.pushButton.clicked.connect(self.upLoadImage)
-        self.uploadButton.clicked.connect(self.creatProgressDialog)
-        
+        self.uploadButton.clicked.connect(self.creatProgressDialog)        
+        #设置label标签
         self.label.setPixmap(QPixmap("./images/Python.png"))
-        #设置label标签的大小
         self.label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        #TableWidget控件设置
         self.tableWidget1.itemClicked.connect(self.clicked)
-        self.tableWidget1.itemDoubleClicked.connect(self.clicked)
-        
+        self.tableWidget1.itemDoubleClicked.connect(self.clicked)       
         self.tableWidget1.setContextMenuPolicy(Qt.CustomContextMenu)  # 右键菜单
         self.tableWidget1.customContextMenuRequested.connect(self.rightMenuShow)
-        
-        # self.status_bar = self.statusBar()
-        # self.statusBar().showMessage("Status: Updated")        
-    
+        """第三个页面设计""" 
+        self.result_folder = 'F:\\广东工业大学\\项目\\02界面\\dev2\\results'  # 结果文件夹路径
+        if not os.path.exists(self.result_folder):
+            os.makedirs(self.result_folder)
+
+    """第一个页面设计
+            内容：上传文件"""    
     #上传图片到系统    
     def upLoadImage(self):
         self.imgPathNames, imgType = QtWidgets.QFileDialog.getOpenFileNames(self, "多文件选择", "/", "图像文件 (*.png *.jpg *.jpeg)")
@@ -86,17 +99,20 @@ class MW(Ui_Form, QWidget):
             self.tableWidget1.setItem(row, 0, nameItem)
         #路径显示   
         self.image_directory = os.path.dirname(str(self.imgPathNames[0]))#变量得是实例变量，否则不显示
-        self.textEdit.setPlainText(f"-----本次处理的图片所在路径:{self.image_directory}-----\n")                     
-    
+        self.textEdit.setPlainText(f"-----本次处理的图片所在路径:{self.image_directory}-----\n")
+        
     #创建进度条    
     def creatProgressDialog(self):
         #进度条部分+创建线程
         #创建线程
         self.uploadThread = UploadThread(self.imgPathNames)
         self.uploadThread.progressUpdated.connect(self.updateProgress)
-        self.uploadThread.finished.connect(self.uploadFinished)            
+        self.uploadThread.finished.connect(self.uploadFinished)
+        """第三个页面设计"""
+        self.uploadThread.resultReceived.connect(self.handle_result)           
         #启动线程
-        self.uploadThread.start()        
+        self.uploadThread.start() 
+        #模态进度条部分       
         if self.imgPathNames and self.uploadThread:       
             self.progress_dialog = QProgressDialog('', '', 0, 100, mw)#需要正确继承父级窗口
             self.progress_dialog.setFixedSize(400, 200)
@@ -107,10 +123,10 @@ class MW(Ui_Form, QWidget):
             self.progress_dialog.canceled.connect(self.cancel_upload)
             self.progress_dialog.setAutoClose(True)#value为最大值时自动关闭
             self.progress_dialog.setValue(0)
-            # self.progress_dialog.setModal(True)  # 设置为模态
+            #self.progress_dialog.setModal(True)  # 设置为模态
             self.progress_dialog.show()#显示出来
     
-    #进度条
+    #上传服务器线程的信号函数
     def updateProgress(self, progress):
         #判断该线程是否存在
         if self.progress_dialog:
@@ -121,10 +137,12 @@ class MW(Ui_Form, QWidget):
         if is_done == 1:
             self.progress_dialog.setValue(100)
             self.textEdit.append("-----发送成功！-----\n")
+            #初始化
             self.imgPathNames = []
             self.uploadThread = None
             
     def cancel_upload(self):
+        #初始化
         self.imgPathNames = []
         if self.uploadThread:
             self.uploadThread.cancel_upload()
@@ -182,6 +200,57 @@ class MW(Ui_Form, QWidget):
             item = QTableWidgetItem(str(row + 1))
             self.tableWidget1.setVerticalHeaderItem(row, item)     
 
+    """第三个页面设计
+            内容:一个TableWidget"""
+    def handle_result(self, result):
+        self.row = 0
+        self.column = 1
+        """测试编辑表格——局限：仅限于一行"""       
+        # 在表格中插入源文件路径
+        self.file_path_item = QTableWidgetItem(self.image_directory)#第一个页面所使用的变量
+        self.tableWidget2.setItem(self.row, self.column, self.file_path_item)
+        
+        # 在表格中插入时间
+        self.column = 2
+        # 获取当前日期时间
+        self.current_datetime = QDateTime.currentDateTime()
+        # 格式化为字符串
+        self.formatted_datetime = self.current_datetime.toString("yyyy-MM-dd hh:mm:ss")
+        self.file_handle_time = QTableWidgetItem(self.formatted_datetime)
+        self.tableWidget2.setItem(self.row, self.column, self.file_handle_time)        
+        
+
+        # 创建打开文件夹按钮
+        self.open_sources_folder_button = QPushButton("源文件")
+        self.open_sources_folder_button.clicked.connect(lambda: self.open_sources_folder(self.row))
+        # 创建打开文件夹按钮
+        self.open_result_folder_button = QPushButton("错误结果")
+        self.open_result_folder_button.clicked.connect(lambda: self.open_result_folder(self.row))
+
+        # 在表格中插入按钮
+        self.button_column = 0
+        self.tableWidget2.setCellWidget(self.row, self.button_column, self.open_sources_folder_button)        
+        self.button_column = 3
+        self.tableWidget2.setCellWidget(self.row, self.button_column, self.open_result_folder_button)
+
+        self.tableWidget2.resizeColumnsToContents()  # 调整列宽以适应内容
+        self.tableWidget2.resizeRowsToContents()  # 调整行高以适应内容
+        
+    def open_sources_folder(self, row):
+        # 获取结果文件夹路径
+        self.sources_folder_path = os.path.abspath(self.image_directory)
+
+        # 使用 QDesktopServices.openUrl() 方法打开文件夹所在路径
+        sources_folder_url = QUrl.fromLocalFile(self.sources_folder_path)
+        QDesktopServices.openUrl(sources_folder_url)        
+        
+    def open_result_folder(self, row):
+        # 获取结果文件夹路径
+        self.result_folder_path = os.path.abspath(self.result_folder)      
+        # 使用 QDesktopServices.openUrl() 方法打开文件夹所在路径
+        result_folder_url = QUrl.fromLocalFile(self.result_folder_path)
+        QDesktopServices.openUrl(result_folder_url)
+        
 
 def get_active_thread_count():
     # 获取当前活动的线程数
